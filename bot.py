32e76d1e804a5ab0a7c947d638e3c0d0e34e37ce
97amigo <pitpit97@yandex.ru>
2019-07-05 20:16:17 +0300

    New telegram bot

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-print('Hello, World!')
+import telebot
+import requests
+
+bot = telebot.TeleBot("627705501:AAEvLXLycmen-FDNlsSbMlni5vWOdepQoY0")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Hey, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
