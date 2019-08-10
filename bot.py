9253129277bd74204563e2fa5beeff2f95f91971
Petr <40881233+97amigo@users.noreply.github.com>
2019-08-10 20:19:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,31 @@
 import telebot
-import requests
+import vk_api
+from vk_api import audio
 
-bot = telebot.TeleBot("627705501:AAEvLXLycmen-FDNlsSbMlni5vWOdepQoY0")
+def vk_case(i, id):
+    REQUEST_STATUS_CODE = 200
+    login = '+79163914689'  # Номер телефона
+    password = '12832pit28!'  # Пароль
+
+    vk_session = vk_api.VkApi(login=login, password=password)
+    vk_session.auth()
+    vk = vk_session.get_api()
+    vk_audio = audio.VkAudio(vk_session)
+
+    return (vk_audio.get(owner_id = int(id))[i]['url'])
+
+mybot_TOKEN = '974770884:AAFkEr5EmeElm4DlGXAns1CgNtaFKrHgK-k'
+
+bot = telebot.TeleBot(mybot_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Hey, how are you doing?")
+    bot.send_message(message.chat.id, "I just give you first five your vk tracks?")
+    bot.send_message(message.chat.id, "Enter the id of page you want to get tracks(it should be opened)")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    for i in range(5):
+        bot.send_audio(chat_id = message.chat.id, audio = vk_case(i, message.text))
 
 bot.polling()
